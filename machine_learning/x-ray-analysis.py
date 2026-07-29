a778524bcbbb34de14d9dc5c4d481b685e5ab746
Anna-Livia Martin <amart001@ad.local.cs.hs-rm.de>
2026-07-29 13:33:21 +0200

    chore: constants in constants file, add test file for analysis, main training file now saving model

--- machine_learning/x-ray-analysis.py
+++ machine_learning/x-ray-analysis.py
@@ -6,17 +6,23 @@ import os
 from nih_train import train, evaluate_on_test
 from constants import CONFIG
 
-if __name__ == "__main__":
-    # Training starten
-    #model = train(CONFIG)
-
-    # Finale Evaluation auf dem Test-Set
-    evaluate_on_test(CONFIG)
-
 def save_model(net, path="./cnn.pt"):
     torch.save(net, path)
+
+def save_model2(model, path="./cnn_state_dict.pt"):
+    torch.save(model.state_dict(), path)
 
 def load_model(path="./cnn.pt"):
     if os.path.isfile(path):
         net = torch.load(path)
     return net
+
+if __name__ == "__main__":
+    # Training starten
+    model = train(CONFIG)
+
+    # Finale Evaluation auf dem Test-Set
+    evaluate_on_test(CONFIG)
+
+    save_model2(model)
+    save_model(model)
