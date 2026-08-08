05f1208ef6e1228e4a3b65e8e894fc085f1c5057
Anna-Livia Martin <119300861+AnnaLiviaMartin@users.noreply.github.com>
2026-08-08 16:25:19 +0200

    chore: delete old server/frontend files and add clean spring boot project including thymeleaf

---
+++ project/src/test/java/de/hsrm/cs/master/medical/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsrm.cs.master.medical.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
