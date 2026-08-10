ccf60b49caaf5ae508513b9553c2841969b50b00
Anna-Livia Martin <119300861+AnnaLiviaMartin@users.noreply.github.com>
2026-08-10 10:47:38 +0200

    chore: cleanup not used parameter

--- project/src/main/java/de/hsrm/cs/master/medical/project/controller/PatientController.java
+++ project/src/main/java/de/hsrm/cs/master/medical/project/controller/PatientController.java
@@ -25,7 +25,7 @@ public class PatientController {
     private HistoryEntryService historyEntryService;
 
     @GetMapping
-    public String list(@RequestParam(defaultValue = "") String q, @RequestParam(defaultValue = "All") String status, @RequestParam(required = false) Long selected, Model model) {
+    public String list(@RequestParam(defaultValue = "") String q, @RequestParam(defaultValue = "All") String status, Model model) {
         List<Patient> patients = patientService.findAll();
 
         if (!q.isBlank()) {
